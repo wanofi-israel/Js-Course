dd8844b43543d19a86e4bec3e9c985e8f4519dd7
Wanofi Israel <wanofiisrael17@gmail.com>
2024-09-23 12:43:04 +0300

    RPS complete

--- RPS.js
+++ RPS.js
@@ -1,3 +1,85 @@
+const choice = ["Rock", "Paper", "Scissors"];
+const rps = document.querySelectorAll(".btn-rps");
+let bot;
+let score;
+let playercho;
+let scoreContainer = document.getElementById("score-container");
+let resultContainer = document.getElementById("result-container");
+let choiceContainer = document.getElementById("choice-container");
+const totalScore={"playerScore":0}
+//getting Bot choice function
+
+const getBotChoice = () => {
+    bot = choice[Math.floor(Math.random() * choice.length)];
+
+    return bot;
+};
+
+const getResult = (botChoice, playerChoice) => {
+    console.log(botChoice, "bot");
+    console.log(playerChoice, "btn");
+    if (botChoice === playerChoice) {
+        score = 0;
+        return score;
+    } else if (playerChoice === "Rock" && botChoice === "Scissors") {
+        score = 1;
+    } else if (playerChoice === "Paper" && botChoice === "Rock") {
+        score = 1;
+    } else if (playerChoice === "Scissors" && botChoice === "Paper") {
+        score = 1;
+    } else {
+        score = -1;
+    }
+    return score;
+};
+
+const showResult = (score, playerChoice, botChoice) => {
+    if (score === -1) {
+        totalScore['playerScore']+=score
+        resultContainer.innerHTML = `You Lose`;
+    } else if (score === 0) {
+        totalScore['playerScore']+=score
+        resultContainer.innerHTML = `You Draw`;
+    } else {
+        totalScore['playerScore']+=score
+        resultContainer.innerHTML = `You Won`;
+    }
+    choiceContainer.innerHTML = `👧 ${playerChoice} vs 🤖 ${botChoice}`;
+    scoreContainer.innerHTML = `${totalScore["playerScore"]}`;
+};
+
+const endGame = () => {
+    scoreContainer.innerHTML = "";
+    choiceContainer.innerHTML = "";
+    resultContainer.innerHTML = "";
+};
+rps.forEach((btn) => {
+    btn.onclick = () => {
+        playercho = btn.value;
+        // calling bot choice function
+        getBotChoice();
+        //calling result checker function
+        getResult(bot, playercho);
+        // check condition if it is a tie
+        showResult(score, playercho, bot);
+        console.log(score, "score");
+    };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -5,7 +87,7 @@
 // const randomSelectorFromArray=()=>{
 //     let fruits=["🍎","🍑","🥕","🌶️","🍐"]
 //     console.log(Math.floor(Math.random()*5));
-    
+
 //     console.log(fruits[Math.floor(Math.random()*4)])
 // }
 
@@ -26,13 +108,3 @@
 // }
 
 // console.log(weatherScorer())
-
-const choice=["Rock","Paper","Scissors"]
-const rps=document.querySelectorAll(".btn-rps")
-let bot
-rps.forEach(btn => {
-
-    btn.onclick=()=>{
-        console.log(btn.value)
-    }
-});
